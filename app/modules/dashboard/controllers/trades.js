b895a31a2a19b3c24b65867650fa5a2d149c85d8
Eduardo Stein Brito <eduardobrito14@gmail.com>
2016-11-23 21:41:19 -0200

    ADD: Working Review of users

--- app/modules/dashboard/controllers/trades.js
+++ app/modules/dashboard/controllers/trades.js
@@ -101,18 +101,18 @@ function ($rootScope, $scope, $state, $location, dashboardService, Flash, $fireb
     $scope.confirmSender = function(trade) {
 
 
-            var ref = firebase.database().ref('trades/'+trade.$id)
-            var tradeDB = $firebaseObject(ref);
-            tradeDB.$loaded().then(function(){
-                tradeDB.senderConfirm = true;
+        var ref = firebase.database().ref('trades/'+trade.$id)
+        var tradeDB = $firebaseObject(ref);
+        tradeDB.$loaded().then(function(){
+            tradeDB.senderConfirm = true;
 
-                tradeDB.$save().then(function(ref) {
-                    Flash.create('success', 'Confirmado o recebimento!', 'large-text');
-                }, function(error) {
-                    console.log("Error:", error);
-                });
+            tradeDB.$save().then(function(ref) {
+                Flash.create('success', 'Confirmado o recebimento!', 'large-text');
+            }, function(error) {
+                console.log("Error:", error);
+            });
 
-            })
+        })
 
 
 
@@ -122,21 +122,87 @@ function ($rootScope, $scope, $state, $location, dashboardService, Flash, $fireb
     $scope.confirmReceiver = function(trade) {
 
 
-            var ref = firebase.database().ref('trades/'+trade.$id)
-            var tradeDB = $firebaseObject(ref);
-            tradeDB.$loaded().then(function(){
-                tradeDB.receiverConfirm = true;
+        var ref = firebase.database().ref('trades/'+trade.$id)
+        var tradeDB = $firebaseObject(ref);
+        tradeDB.$loaded().then(function(){
+            tradeDB.receiverConfirm = true;
 
-                tradeDB.$save().then(function(ref) {
-                    Flash.create('success', 'Confirmado o recebimento!', 'large-text');
-                }, function(error) {
-                    console.log("Error:", error);
-                });
+            tradeDB.$save().then(function(ref) {
+                Flash.create('success', 'Confirmado o recebimento!', 'large-text');
+            }, function(error) {
+                console.log("Error:", error);
+            });
 
-            })
+        })
 
 
 
     }
+
+    $scope.loadComments = function(trade) {
+        var ref = firebase.database().ref('trades/'+trade.$id+'/comments')
+        var commentList = $firebaseArray(ref);
+        commentList.$loaded().then(function(){
+            $scope.comments = commentList;
+        });
+    }
+
+
+
+    $scope.comment = function(trade, message) {
+
+        console.log(message);
+        var ref = firebase.database().ref('trades/'+trade.$id+'/comments')
+        var comments = $firebaseArray(ref);
+        var commentAdd = {'user': $rootScope.user.Username , 'message': message}
+        console.log(commentAdd);
+        comments.$loaded().then(function(){
+            // add an item
+            comments.$add(commentAdd).then(function(ref) {
+                Flash.create('success', 'Comentário enviado!', 'large-text');
+
+            });
+        });
+    }
+
+    $scope.review = function(trade, user, message) {
+
+        console.log(message);
+        var ref = firebase.database().ref('users/'+user+'/ratings')
+        var ratings = $firebaseArray(ref);
+        var rateAdd = {'user': $rootScope.user.Username , 'message': message}
+        console.log(rateAdd);
+        ratings.$loaded().then(function(){
+            // add an item
+            ratings.$add(rateAdd).then(function(ref) {
+                Flash.create('success', 'Avaliação enviada!', 'large-text');
+
+            });
+        });
+        var ref = firebase.database().ref('trades/'+trade.$id)
+        var tradeDB = $firebaseObject(ref);
+
+        tradeDB.$loaded().then(function(){
+            var done = []
+            if(tradeDB.doneUser != undefined)
+            done = tradeDB.doneUser;
+            done.push($rootScope.userDB.uid)
+            tradeDB.doneUser = done;
+
+            tradeDB.$save().then(function(ref) {
+                // Test to see if both users have compleated the trade
+                if(trade.doneUser.length == 2){
+                    tradeDB.$remove();
+                    Flash.create('success', 'Ambos se avaliaram. Troca finalizada!', 'large-text');
+                }
+            }, function(error) {
+                console.log("Error:", error);
+            });
+
+        })
+    }
+
+
+
 
 }]);
